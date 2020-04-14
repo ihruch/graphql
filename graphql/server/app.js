62c6a0f92988b759905d2f4ef6b634927711a7fa
“Ihor” <ihruch@gmail.com>
2020-04-14 21:03:32 +0300

    Adding Mongoose Models & Updating Resolve

--- graphql/server/app.js
+++ graphql/server/app.js
@@ -1,15 +1,21 @@
 const express = require('express');
 const graphqlHTTP = require('express-graphql');
-const  schema = require('./../schema/schema');
+const schema = require('./../schema/schema');
+const mongoose = require('mongoose');
 
-
+mongoose.connect('mongodb+srv://igor:123@cluster0-omo0j.mongodb.net/graphql?retryWrites=true&w=majority', { useNewUrlParser: true });
+// mongodb+srv://igor:<password>@cluster0-omo0j.mongodb.net/test?retryWrites=true&w=majority
 const app = express();
 const PORT = 3000;
 
 app.use('/graphql', graphqlHTTP({
     schema,
     graphiql: true,
-  }));
+}));
+
+const dbConnection = mongoose.connection;
+dbConnection.on('error', err => console.log(`Connection error: ${err}`));
+dbConnection.once('open', () => console.log('Connected to DB!'));
 
 app.listen(PORT, err => {
     err? console.error(error) : console.log('Server started')
